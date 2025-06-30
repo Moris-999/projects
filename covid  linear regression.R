01d11c790c83eb82cabf568cb270339b59d3ef2b
Moris Muchuki Maina <165458509+Moris-999@users.noreply.github.com>
2025-06-30 07:17:33 +0300

    Update covid  linear regression.R

--- covid  linear regression.R
+++ covid  linear regression.R
@@ -1,5 +1,5 @@
 ### visualize comfirmired case , death rates and recovered cases
-### build a model for this case
+### build a  model for this case
 
 ##load in the libraries
 library(ggplot2)
@@ -39,6 +39,53 @@ ggplot(covid_long, aes(x = value, fill = variable)) +
        y = "Frequency") +
   theme_minimal()
 
+top_countries <- covid %>%
+  group_by(Country.Region) %>%
+  summarise(Deaths = sum(Deaths, na.rm = TRUE)) %>%
+  arrange(desc(Deaths)) %>%
+  slice(1:25)
+
+ggplot(top_countries, aes(x = reorder(Country.Region, Deaths), y = Deaths, fill = Country.Region)) +
+  geom_col(show.legend = FALSE) +
+  theme_light() +
+  labs(x = "Country", y = "Deaths") +
+  coord_flip() +
+  theme(axis.text.y = element_text(size = 14))
+
+### usa is leading with number of deaths with Netherlands with the list top countries
+### 25
+
+##Top countries with confirmed cases
+top_countries <- covid %>%
+  group_by(Country.Region) %>%
+  summarise(Confirmed = sum(Confirmed, na.rm = TRUE)) %>%
+  arrange(desc(Confirmed)) %>%
+  slice(1:25)
+
+
+ggplot(top_countries, aes(x = reorder(Country.Region,Confirmed), y = Confirmed, fill = Country.Region)) +
+  geom_col(show.legend = FALSE) +
+  theme_light() +
+  labs(x = "Country", y = "Confirmed Cases") +
+  coord_flip() +
+  theme(axis.text.y = element_text(size = 14))
+
+###top 25 countries usa is still leading with confirmed cases and belgium at the end
+
+  ### top 25 countries with recovered cases
+top_countries <- covid %>%
+  group_by(Country.Region) %>%
+  summarise(Recovered = sum(Recovered, na.rm = TRUE)) %>%
+  arrange(desc(Recovered)) %>%
+  slice(1:25)
+
+ggplot(top_countries, aes(x = reorder(Country.Region,Recovered), y = Recovered, fill = Country.Region)) +
+  geom_col(show.legend = FALSE) +
+  theme_light() +
+  labs(x = "Country", y = "Recovered Cases") +
+  coord_flip() +
+  theme(axis.text.y = element_text(size = 14))
+### India has a higher number of people how have recovered from covid
 
 # Select relevant columns
 covid_numeric <- covid[, c("Confirmed", "Deaths", "Recovered")]
